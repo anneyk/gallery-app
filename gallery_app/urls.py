77e0b55aef82eb37206ba80ad60ac535f0eb172c
Annetkhavere <anneywan5@gmail.com>
2022-05-30 00:32:23 +0300

    register model classes

--- gallery_app/urls.py
+++ gallery_app/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^photos/',include('photos.urls'))
+    url(r'',include('photos.urls'))
 ]
--- photos/admin.py
+++ photos/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from .models import Location, Category, Image
 
 # Register your models here.
-admin.site.register(Location)
-admin.site.register(Category)
-admin.site.register(Image)
+class LocationAdmin(admin.ModelAdmin):
+  list_display = ['location_name']
+  ordering = ['location_name']
+
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ['category_name']
+  ordering = ['category_name']
+
+class ImageAdmin(admin.ModelAdmin):
+  list_display = ['image_name','image_description','image_category','image_location','published_date']
+
+admin.site.register(Location,LocationAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Image,ImageAdmin)
