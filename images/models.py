c759a756441428c0e6b1c536f8f4eb8882afb193
Annetkhavere <anneywan5@gmail.com>
2022-05-31 00:27:55 +0300

    added images to admin

---
+++ images/models.py
@@ -0,0 +1,96 @@
+from django.db import models
+
+# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length =80, unique='True',null=True)
+    
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Location"
+        verbose_name_plural = "Locations"
+        
+        
+    def __str__(self):
+        return self.name
+    
+    def save_location(self):
+        self.save()
+        
+    
+    def delete_location(self):
+        self.delete()
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length =80, unique='True', null=True)
+    
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
+    
+    
+    def save_category(self):
+            self.save()
+    
+    def delete_category(self):
+        self.delete()
+    
+    
+
+class Image(models.Model):
+    name = models.CharField(max_length =80,null=True)
+    description = models.TextField(null=True)
+    location = models.ForeignKey(Location, related_name='location', on_delete=models.DO_NOTHING,null=True)
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.DO_NOTHING,null=True)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    photo = models.ImageField(upload_to = 'photos/',null=True)
+    
+    class Meta:
+        ordering = ["pub_date"]
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
+    
+    def summary(self):
+        return self.description[:100]+" ......"
+    
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+    
+    def save_image(self):
+        self.save()
+    
+    
+    def delete_image(self):
+        self.delete()
+    
+    def update_image(self):
+        pass
+        
+
+    def get_image_by_id(cls):
+        images = cls.objects.get(pk=id)
+        return images
+    
+    def search_image(category):
+        pass
+    
+    def filter_by_location(location):
+        pass
+    
+    
+    @classmethod
+    def search_by_category(cls,search_term):
+        searched_images = cls.objects.filter(category__icontains = search_term)
+        return searched_images
+    
+ 
+    
+    
+    
+    
---
+++ images/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns =[
+    # url(r'^$', views.welcome, name = 'welcome'),
+    url(r'^$', views.index, name = 'home'),
+    url(r'^gallery/$', views.gallery, name = 'gallery'),
+    url(r'^gallery/<int:image_id>/$', views.single_image_details, name='image_details'),
+    url(r'^about/$', views.about_us, name = 'about'),
+    url(r'^search/', views.search_category, name = 'search_category'),
+    url(r'^category/sports/$', views.sports, name = 'sports'),
+    url(r'^category/technology/$', views.technology, name = 'technology'),
+    url(r'^category/nature/$', views.nature, name = 'nature'),
+    url(r'^category/entertainment/$', views.entertainment, name = 'entertainment'),
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
