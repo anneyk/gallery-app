c18feef843901a8da385712d74f90da15a042df7
Annetkhavere <anneywan5@gmail.com>
2022-06-02 14:17:51 +0300

    update url paths

--- images/urls.py
+++ images/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns =[
     # url(r'^$', views.welcome, name = 'welcome'),
-    url(r'^$', views.index, name = 'home'),
-    url(r'^gallery/$', views.gallery, name = 'gallery'),
-    url(r'^gallery/<int:image_id>/$', views.single_image_details, name='image_details'),
-    url(r'^about/$', views.about_us, name = 'about'),
-    url(r'^search/', views.search_category, name = 'search_category'),
-    url(r'^category/sports/$', views.sports, name = 'sports'),
-    url(r'^category/technology/$', views.technology, name = 'technology'),
-    url(r'^category/nature/$', views.nature, name = 'nature'),
-    url(r'^category/entertainment/$', views.entertainment, name = 'entertainment'),
+    path('', views.index, name = 'home'),
+    path('gallery/', views.gallery, name = 'gallery'),
+    path('gallery/<int:image_id>/', views.single_image_details, name='image_details'),
+    path('about/', views.about_us, name = 'about'),
+    path('search/', views.search_category, name = 'search_category'),
+    path('category/sports/', views.sports, name = 'sports'),
+    path('category/technology/', views.technology, name = 'technology'),
+    path('category/nature/', views.nature, name = 'nature'),
+    path('category/entertainment/', views.entertainment, name = 'entertainment'),
 
 ]
 if settings.DEBUG:
